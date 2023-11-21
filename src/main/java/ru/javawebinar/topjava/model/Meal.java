e123d98fa8a68550dffe0dac53bf1d24f8f132a0
Andrew <andfrewluferenko@icloud.com>
2023-11-21 23:34:20 +0300

    added field user id, added set and get method for field user id, added user id to object Meal in MealsUtil, added user id to object Meal in MealServlet, do not look User Servlet

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -17,6 +17,22 @@ public class Meal {
         this.calories = calories;
     }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
     public LocalDateTime getDateTime() {
         return dateTime;
     }
