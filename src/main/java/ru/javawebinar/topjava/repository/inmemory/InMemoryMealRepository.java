3ce8545322c8b37f51fee5557c17ed89083ab6b5
Andrew <andfrewluferenko@icloud.com>
2023-11-22 10:29:25 +0300

    2_5_add_spring_context

---
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -0,0 +1,57 @@
+package ru.javawebinar.topjava.repository.inmemory;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.repository.MealRepository;
+import ru.javawebinar.topjava.util.MealsUtil;
+
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+
+public class InMemoryMealRepository implements MealRepository {
+    private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+    private final AtomicInteger counter = new AtomicInteger(0);
+
+    {
+        MealsUtil.meals.forEach(this::save);
+    }
+
+    @Override
+    public Meal save(Meal meal) {
+        if (meal.isNew()) {
+            meal.setId(counter.incrementAndGet());
+            repository.put(meal.getId(), meal);
+            return meal;
+        }
+        // handle case: update, but not present in storage
+        return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    }
+
+    @Override
+    public boolean delete(int id, int userId) {
+        return repository.remove(id) != null;
+    }
+
+    @Override
+    public Meal get(int id, int userId) {
+        if (repository.get(id) == null) {
+            return null;
+        }
+        Meal meal = repository.get(id);
+        return meal.getUserId() == userId ? meal : null;
+    }
+
+    @Override
+    public Collection<Meal> getAll(int userId) {
+        return repository
+                .values()
+                .stream()
+                .filter(meal -> meal.getUserId() == userId)
+                .sorted((Comparator.comparing(Meal::getDateTime)))
+                .collect(Collectors.toList());
+    }
+}
+
