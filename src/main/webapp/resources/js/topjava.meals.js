db461b38063ed6202d9d49d48632ee1459ddb056
Andrew <andfrewluferenko@icloud.com>
2024-01-30 12:08:12 +0300

    HW_8 corrected

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -1,18 +1,106 @@
-var mealsAjaxUrl = "profile/meals";
+// const mealsAjaxUrl = "profile/meals";
+//
+// var ctx = {
+//     ajaxUrl: mealsAjaxUrl
+// };
+//
+// $(function () {
+//     ctx = {
+//         ajaxUrl: "profile/meals",
+//         datatableApi: $("#datatable").DataTable({
+//             "paging": false,
+//             "info": true,
+//             "columns": [
+//                 {
+//                     "data": "dateTime"
+//                 },
+//                 {
+//                     "data": "description"
+//                 },
+//                 {
+//                     "data": "calories"
+//                 },
+//                 {
+//                     "defaultContent": "Edit",
+//                     "orderable": false
+//                 },
+//                 {
+//                     "defaultContent": "Delete",
+//                     "orderable": false
+//                 }
+//             ],
+//             "order": [
+//                 [
+//                     0,
+//                     "desc"
+//                 ]
+//             ]
+//         }),
+//         updateTable: updateFiltered
+//     };
+//     makeEditable();
+// });
+//
+// function updateFiltered() {
+//     $.ajax({
+//         type: "GET",
+//         url: "/profile/meals/filter",
+//         data: $("#filter").serialize()
+//     }).done(updateTableByData())
+// }
+//
+// $(function () {
+//     $("#refreshButton").click(function () {
+//         $.ajax({
+//             url: mealsAjaxUrl,
+//             cache: false,
+//             success: function (html) {
+//                 $("#filter").html(html);
+//             }
+//         });
+//     });
+// });
 
-var ctx = {
-    ajaxUrl: mealsAjaxUrl
+
+
+
+const mealAjaxUrl = "profile/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl,
+    updateTable: function () {
+        $.ajax({
+            type: "GET",
+            url: mealAjaxUrl + "filter",
+            data: $("#filter").serialize()
+        }).done(updateTableByData);
+    }
 };
 
+function clearFilter() {
+    $("#filter")[0].reset();
+    $.get(mealAjaxUrl, updateTableByData);
+}
+
 $(function () {
-    ctx = {
-        ajaxUrl: "profile/meals",
-        datatableApi: $("#datatable").DataTable({
+    makeEditable(
+        $("#datatable").DataTable({
+            "ajax": {
+                "url": mealAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
                 {
-                    "data": "dateTime"
+                    "data": "dateTime",
+                    "render": function (data, type, row) {
+                        if (type === "display") {
+                            return date.replace('T',  '').substring(0, 16);
+                        }
+                        return date;
+                    }
                 },
                 {
                     "data": "description"
@@ -21,12 +109,14 @@ $(function () {
                     "data": "calories"
                 },
                 {
-                    "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderEditBtn
                 },
                 {
-                    "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderEditBtn
                 }
             ],
             "order": [
@@ -34,29 +124,12 @@ $(function () {
                     0,
                     "desc"
                 ]
-            ]
-        }),
-        updateTable: updateFiltered
-    };
-    makeEditable();
-});
-
-function updateFiltered() {
-    $.ajax({
-        type: "GET",
-        url: "/profile/meals/filter",
-        data: $("#filter").serialize()
-    }).done(updateTableByData())
-}
-
-$(function () {
-    $("#refreshButton").click(function () {
-        $.ajax({
-            url: mealsAjaxUrl,
-            cache: false,
-            success: function (html) {
-                $("#filter").html(html);
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                if (!data.enabled){
+                    $(row).attr("data-meal-excess", data.excess)
+                }
             }
-        });
-    });
+        })
+    );
 });
